737d15de0b505b19270e67d50a98bd534c5bdd8b
Psage23 <pamelasage3@gmail.com>
2019-12-03 16:48:29 -0700

    data is return

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import UserCard from './components/usercard'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    coders: []
+  }
+  
+  componentDidMount() {
+    axios.get('https://api.github.com/users/Psage23')
+    .then(response => {
+      console.log(response);
+      this.setState({coders: response.data})
+    })
+  }
+
+  componentDidUpdate() {
+
+  }
+
+  render() {
+    console.log('render is working')
+    return (
+      <div className = "App">
+        <h1>Best GitHub Users</h1>
+        <div className = "profile-cards">
+          <UserCard />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ user-card/src/components/usercard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const UserCard = () => {
+    return (
+        <div class="container">
+            <div class="header">
+            <img src="./public/lambdalogo.png" alt="Lambda Logo"/>
+            <p>❤️'s</p>
+            <img src="./public/githublogo.png" alt="GitHub Logo" />
+        </div>
+        <div class="cards"></div>
+        </div>
+    )
+}
+
+export default UserCard;
