ffdc2e8d461aa30c7df9e1088218db23a9bcdd55
Psage23 <pamelasage3@gmail.com>
2019-12-03 17:27:35 -0700

    modified trying to setup profile cards

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-import UserCard from './components/usercard'
+import UserCard from './components/usercard';
+
 import './App.css';
 
 class App extends React.Component {
@@ -25,6 +26,14 @@ class App extends React.Component {
     return (
       <div className = "App">
         <h1>Best GitHub Users</h1>
+        {/* <div class="container">
+            <div className="header">
+            <img src={lambdalogo} alt="Lambda Logo"/>
+            <span role="img" aria-label="heart">❤️'s</span>
+            <img src="./public/githublogo.png" alt="GitHub Logo" />
+        </div>
+        <div class="cards"></div>
+        </div> */}
         <div className = "profile-cards">
           <UserCard />
         </div>
---
+++ user-card/src/components/users.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Users = () => {
+    return (
+        <div>
+            <h2>name</h2>
+            <p>Bio</p>
+        </div>
+    )
+}
+
+export default Users;
